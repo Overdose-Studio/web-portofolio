f12455ceae2f705ae990d8896084baf8f89d5357
Ubean <76271600+ubeann@users.noreply.github.com>
2023-04-06 02:22:55 +0700

    BE: Install `@fastify/helmet` on app for HTTP headers security (#3)
    * Install Fastify Helmet plugin for HTTP headers security
    * Integrates `@fastify/helmet` on app

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,15 @@
 // Import dependencies
 import Fastify from 'fastify';
+import helmet from '@fastify/helmet';
 
 // Import modules
 import logger from './utils/logger';
 
 // Create fastify app
 const app = Fastify({ logger: logger });
+
+// Use plugins on the app
+app.register(helmet);                                       // Set security HTTP headers
 
 // Export app
 export default app;
