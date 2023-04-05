ec230faf668c5cad11a0638602dc380b80a863af
Ubean <76271600+ubeann@users.noreply.github.com>
2023-04-06 01:34:18 +0700

    BE: Install Fastify web framework (#1)
    * Create .gitignore
    * Delete .gitkeep
    * Create .env.example for server configuration
    * Implements Fastify Framework

---
+++ server/src/index.ts
@@ -0,0 +1,29 @@
+// Import dependencies
+import colors from "colors";
+import dotenv from "dotenv";
+
+// Import modules
+import app from "./app";
+
+// Load environment variables
+dotenv.config();
+
+// Get environment variables
+const port = process.env.PORT ?? "5000";
+const host = process.env.HOST ?? "localhost";
+const debugMode = process.env.DEBUG_MODE === 'true';
+
+// Start server on port
+app.listen({ port: parseInt(port), host: host }, function (err) {
+	// Handle errors
+	if (err) {
+	  app.log.error(err)	// Log error
+	  console.error(err)	// Console error
+	  process.exit(1)		// Exit process
+	}
+
+	// Show server address
+	if (debugMode) {
+		console.log("Server running on port:", colors.green(`${port}`));
+	}
+})
