2c8d971394dbefc0815f61c30777000d8c872ba2
Ubean <76271600+ubeann@users.noreply.github.com>
2023-04-06 02:09:42 +0700

    BE: Install Pino logger on app (#2)
    * Add configuration for logging to .env.example file
    * Create `logs` directory
    * Create Pino logger modules for app
    * Integrates Pino logger on app

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,11 @@
 // Import dependencies
 import Fastify from 'fastify';
 
+// Import modules
+import logger from './utils/logger';
+
 // Create fastify app
-const app = Fastify();
+const app = Fastify({ logger: logger });
 
 // Export app
 export default app;
