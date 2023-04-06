d9afb01295a5134a1b296c776c2e42c30195bf53
Ubean <76271600+ubeann@users.noreply.github.com>
2023-04-06 19:14:27 +0700

    BE: Create database connection to MongoDB using `mongoose` (#17)
    * Install `mongoose` as a database plugin
    * Create database interface
    * Add configuration for database to `.env.example` file
    * Create config-database.ts
    * Integrates database connection on app

---
+++ server/src/database/config-database.ts
@@ -0,0 +1,61 @@
+// Import dependencies
+import mongoose from "mongoose";
+import colors from "colors";
+import dotenv from "dotenv";
+
+// Import interfaces
+import Database from "../interfaces/database-interface";
+
+// Load environment variables
+dotenv.config();
+
+// Get environment variables
+const debug = process.env.DEBUG_MODE === 'true';
+const databaseENV = {
+    name: process.env.DB_NAME ?? "test",
+    URI: process.env.DB_URI ?? "mongodb://localhost:27017",
+}
+
+// Set strictQuery to false
+mongoose.set("strictQuery", false);
+
+// Create database connection
+const database: Database = {
+    // Connect to database
+    connect: async () => {
+        try {
+            const conn = await mongoose.connect(`${databaseENV.URI}/${databaseENV.name}`);
+            if (debug) {
+                console.log(`Connected to database:`, colors.green(conn.connection.name), `(${conn.connection.host})`);
+            }
+        } catch (error) {
+            console.error(error);
+            process.exit(1);
+        }
+    },
+
+    // Get all collections
+    getCollections: async () => {
+        try {
+            const collections = await mongoose.connection.db.collections();
+            return collections;
+        } catch (err) {
+            console.error(err);
+            process.exit(1);
+        }
+    },
+
+    // Get connection
+    getConnection: async () => {
+        try {
+            const conn = await mongoose.connection;
+            return conn;
+        } catch (err) {
+            console.error(err);
+            process.exit(1);
+        }
+    }
+};
+
+// Export database connection
+export default database;
