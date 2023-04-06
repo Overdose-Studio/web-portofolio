e6829cb1cc46347396caf8509dd56f8d095e9602
Ubean <76271600+ubeann@users.noreply.github.com>
2023-04-07 01:27:20 +0700

    BE: Fix bug log file name when rotate file stream (#20)
    * Reorder log configuration on .env.example
    * Improve log filename

--- server/src/utils/logger.ts
+++ server/src/utils/logger.ts
@@ -10,12 +10,12 @@ dotenv.config();
 
 // Get environment variables
 const log = {
-    level: process.env.LOG_LEVEL ?? "info",
-    file: process.env.LOG_FILE ?? "access.log",
-    directory: process.env.LOG_DIRECTORY ?? "logs",
-    maxFiles: process.env.LOG_MAX_FILES ?? "7",
     compress: process.env.LOG_COMPRESS === "true",
+    directory: process.env.LOG_DIRECTORY ?? "logs",
+    file: process.env.LOG_FILE ?? "access.log",
     interval: process.env.LOG_INTERVAL ?? "1d",
+    level: process.env.LOG_LEVEL ?? "info",
+    maxFiles: process.env.LOG_MAX_FILES ?? "7",
     timezone: process.env.LOG_TIMEZONE ?? "Asia/Jakarta"
 };
 
@@ -23,19 +23,20 @@ const log = {
 const pad = (num: number) => {
     return num < 10 ? "0" + num : num;
 };
-const filename = (time: Date = new Date()) => {
-  var year = time.getFullYear();
-  var month = pad(time.getMonth() + 1);
-  var day = pad(time.getDate());
-  return `${year}-${month}-${day}-${log.file}`;
+const filename = (time: any) => {
+    if (!time) return log.file;
+    var year = time.getFullYear();
+    var month = pad(time.getMonth() + 1);
+    var day = pad(time.getDate());
+    return `${year}-${month}-${day}-${log.file}`;
 };
 
 // Create write stream for log files (rotate daily)
-const accessLogStream = createStream(filename(), {
+const accessLogStream = createStream(filename, {
     interval: log.interval,                                  // rotate daily
     path: path.join(__dirname, `../../${log.directory}`),    // log files are stored in this directory
     maxFiles: parseInt(log.maxFiles),                        // keep back copies
-    compress: log.compress ? "gzip" : false                  // compress rotated files
+    compress: log.compress                                   // compress rotated files
 });
 
 // Create logger
@@ -49,7 +50,7 @@ const logger = pino({
                 // Write to log file
                 level: log.level,
                 target: "pino/file",
-                options: { destination: `${log.directory}/${filename()}` }
+                options: { destination: `${log.directory}/${log.file}` }
             },
             {
                 // Write to console
