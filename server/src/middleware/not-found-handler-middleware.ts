e4941a416dbe3b6e2e8c1ccba0bc5832e5901179
Ubean <76271600+ubeann@users.noreply.github.com>
2023-04-06 13:43:54 +0700

    BE: Create not found handler (#12)
    * Create not found handler middleware
    * Integrates not found handler on app

---
+++ server/src/middleware/not-found-handler-middleware.ts
@@ -0,0 +1,32 @@
+//-------------------------------------------------------------------------------
+//* Description: Middleware to handle 404 errors (Not Found)
+//*              This middleware is used to handle 404 errors (Not Found)
+//*              This middleware is set in src/app.ts
+//-------------------------------------------------------------------------------
+
+// Import dependencies
+import { FastifyRequest, FastifyReply } from 'fastify';
+
+// Import models
+import ErrorAPI from '../models/error-api-model';
+import StatusAPI from '../models/status-api-model';
+
+// Create 404 handler middleware
+const notFoundHandler = (request: FastifyRequest, reply: FastifyReply) => {
+    // Set response status code
+    reply.status(404);
+
+    // Set response body
+    reply.json({
+        status: StatusAPI.FAILED,
+        status_code: 404,
+        code: ErrorAPI.NOT_FOUND,
+        message: `The requested resource '${request.url}' could not be found`,
+        meta: {
+            path: request.url,
+        }
+    });
+}
+
+// Export module
+export default notFoundHandler;
