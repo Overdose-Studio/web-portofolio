e1a50d7c0471e07454d0c7477cc743689989f114
ubeann <76271600+ubeann@users.noreply.github.com>
2023-04-06 13:11:19 +0700

    Create json-standard-interface.ts

---
+++ server/src/interfaces/json-standard-interface.ts
@@ -0,0 +1,18 @@
+// Import models
+import StatusAPI from '../models/status-api-model';
+
+// Create JSON standard interface
+interface JSON_Standard {
+    status: StatusAPI;          // Status of the response
+    status_code: number;        // Status code of the response
+    message: string;            // Message of the response
+    data?: any;                 // Data object (e.g. user object)
+    meta?: any;                 // Meta object (e.g. pagination object)
+    links?: any;                // Links object (e.g. pagination links)
+    included?: any;             // Included object (e.g. user object)
+    code?: string;              // Code of the response (e.g. error code)
+    stack?: any;                // Stack of the response (e.g. error stack)
+}
+
+// Export interface
+export default JSON_Standard;
