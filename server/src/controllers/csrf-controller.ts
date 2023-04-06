7ff276f270a5a2daf839d09b8dbb3294135d3e33
ubeann <76271600+ubeann@users.noreply.github.com>
2023-04-06 16:26:15 +0700

    Create CSRF controller to generate token

---
+++ server/src/controllers/csrf-controller.ts
@@ -0,0 +1,44 @@
+// Import dependencies
+import dotenv from 'dotenv';
+import { FastifyRequest, FastifyReply} from 'fastify';
+
+// Import models
+import StatusAPI from '../models/status-api-model';
+
+// Load environment variables
+dotenv.config();
+
+// Get environment variables
+const production = process.env.NODE_ENV === 'production';
+
+// @desc   Generate CSRF token
+// @route  GET /csrf-token
+// @access Public
+export const generateCSRFToken = async (request: FastifyRequest, reply: FastifyReply) => {
+    // Check on production
+    if (!production) {
+        // Set response body
+        reply.status(403);
+        reply.error = {
+            status: StatusAPI.ERROR,
+            data: {
+                csrf_token: 'CSRF token can only be generated on production'
+            }
+        };
+        throw new Error('CSRF token can only be generated on production');
+    }
+
+    // Generate CSRF token
+    const token = await reply.generateCsrf();
+
+    // Set response body
+    reply.status(200);
+    reply.json({
+        status: StatusAPI.SUCCESS,
+        status_code: 200,
+        message: 'CSRF token generated successfully',
+        data: {
+            csrf_token: token
+        }
+    });
+}
