f44482396a9037ee6ad3229b174e40a6873a3d1d
ubeann <76271600+ubeann@users.noreply.github.com>
2023-04-06 13:32:27 +0700

    Create error-attribute-extension.ts

---
+++ server/src/extensions/error-attribute-extension.ts
@@ -0,0 +1,26 @@
+// This file is used to extend the FastifyReply interface with the error property
+
+// Import dependencies
+import 'fastify';
+
+// Import models
+import ErrorAPI from '../models/error-api-model';
+import StatusAPI from '../models/status-api-model';
+
+// Create Error Properties interface
+export type ErrorProperties = {
+    status?: StatusAPI;     // StatusAPI: SUCCESS, FAILED, ERROR
+    type?: ErrorAPI;        // ErrorAPI: ERR_VALIDATION, ERR_AUTHENTICATION
+    data?: any;             // Data object (e.g. user object)
+    meta?: any;             // Meta object (e.g. pagination object)
+    links?: any;            // Links object (e.g. pagination links)
+    included?: any;         // Included object (e.g. user object)
+};
+
+// Extend FastifyReply interface
+declare module 'fastify' {
+    // Create error reply interface
+    interface FastifyReply {
+        error: ErrorProperties;
+    }
+}
