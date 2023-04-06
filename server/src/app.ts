e45679ce78470ef79b597950bceaa29ddf36391c
ubeann <76271600+ubeann@users.noreply.github.com>
2023-04-06 12:24:16 +0700

    Integrates cookie parser plugin on app

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,44 +1,20 @@
 // Import dependencies
 import Fastify from 'fastify';
-import cookie from '@fastify/cookie';
-import dotenv from 'dotenv';
 import helmet from '@fastify/helmet';
 
 // Import modules
 import logger from './utils/logger';
 
 // Import plugins
+import cookieParser from './plugins/cookie-parser-plugin';
 import formBodyParser from './plugins/form-body-parser-plugin';
-
-// Load environment variables
-dotenv.config();
-
-// Get environment variables
-const cookieENV = {
-    domain: process.env.COOKIE_DOMAIN ?? 'localhost',
-    httpOnly: process.env.COOKIE_HTTP_ONLY === 'true',
-    maxAge: parseInt(process.env.COOKIE_MAX_AGE ?? '86400000'),
-    secure: process.env.COOKIE_SECURE === 'true',
-    secret: process.env.COOKIE_SECRET ?? 'secret',
-    signed: process.env.COOKIE_SIGNED === 'true',
-};
 
 // Create fastify app
 const app = Fastify({ logger: logger });
 
 // Use plugins on the app
 app.register(helmet);                                       // Set security HTTP headers
-app.register(cookie, {                                      // Parse and set cookies
-    secret: cookieENV.secret,                               //--- Set cookie secret
-    parseOptions: {                                         //--- Set parse options
-        signed: cookieENV.signed,                           //------ Set cookie to signed
-        secure: cookieENV.secure,                           //------ Set cookie to only valid for HTTPS when production
-        httpOnly: cookieENV.httpOnly,                       //------ Set cookie to only valid for HTTP
-        sameSite: 'strict',                                 //------ Set cookie to only valid for same site
-        domain: cookieENV.domain,                           //------ Set cookie domain to localhost when production
-        maxAge: cookieENV.maxAge,                           //------ Set cookie max age to default 1 day
-    }
-});
+app.register(cookieParser);                                 // Parse and set cookies
 app.register(formBodyParser);                               // Parse and set form data
 
 // Export app
