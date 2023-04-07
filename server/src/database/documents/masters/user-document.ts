59418009592c1bd0a5976e07da67b0a6709b6e97
ubeann <76271600+ubeann@users.noreply.github.com>
2023-04-07 12:21:11 +0700

    Add authentication methods on user model

--- server/src/database/documents/masters/user-document.ts
+++ server/src/database/documents/masters/user-document.ts
@@ -1,12 +1,26 @@
 // Import dependencies
 import { Document } from "mongoose";
+import { FastifyReply } from "fastify";
+
+// Import interfaces
+import TokenPayloadJWT from "../../../interfaces/jwt-payload-interface";
 
 // Create a user document
 interface IUser extends Document {
+    // Properties
     name: string;
     email: string;
     role: string;
     password: string;
+    created_at: Date;
+    updated_at: Date;
+    deleted_at: Date;
+
+    // Methods
+    comparePassword(password: string): Promise<boolean>;
+    getPayloadJWT(): TokenPayloadJWT;
+    generateAccessToken(reply: FastifyReply): Promise<string>;
+    generateRefreshToken(reply: FastifyReply): Promise<string>;
 }
 
 // Export user document
