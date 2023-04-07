2e4b95ec0a013b95e6183489bedae41bbf42665b
ubeann <76271600+ubeann@users.noreply.github.com>
2023-04-07 22:45:53 +0700

    Update JSON command on `fastify`

--- server/src/plugins/json-response-plugin.ts
+++ server/src/plugins/json-response-plugin.ts
@@ -9,8 +9,15 @@ import JSON_Standard from '../interfaces/json-standard-interface';
 const jsonResponse: FastifyPluginAsync = plugin(async (fastify: FastifyInstance) => {
     // Decorate the fastify instance with a custom function
     fastify.decorateReply('json', async function (this: FastifyReply, params: JSON_Standard) {
-        // Send response using standard JSON
-        this.send(params);
+        // Get status response
+        const { status, ...rest } = params;
+
+        // Send response using standard JSON format
+        this.send({
+            status: status,
+            status_code: rest.status_code ?? this.statusCode,
+            ...rest
+        });
     });
 });
 
