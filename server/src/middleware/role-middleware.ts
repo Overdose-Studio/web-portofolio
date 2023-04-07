e40f8dfafe3b5ef324caee8a7ad21ce9811d1c2c
ubeann <76271600+ubeann@users.noreply.github.com>
2023-04-07 12:32:54 +0700

    Create role middleware

---
+++ server/src/middleware/role-middleware.ts
@@ -0,0 +1,24 @@
+// Import dependencies
+import { FastifyRequest, FastifyReply } from 'fastify';
+
+// Import enums
+import ErrorAPI from '../database/enums/error-api-enum';
+import StatusAPI from '../database/enums/status-api-enum';
+import UserRole from '../database/enums/user-role-enum';
+
+// Create auth middleware to check if user is authenticated
+const roleMiddleware = (...role: UserRole[]) => async (request: FastifyRequest, reply: FastifyReply) => {
+    // Check if user role is allowed
+    if (!role.includes(request.user.role as UserRole)) {
+        // Send error response
+        reply.status(403);
+        reply.error = {
+            status: StatusAPI.FAILED,
+            type: ErrorAPI.AUTHORIZATION,
+            data: { role: 'Role not allowed' }
+        }
+        throw new Error('Authorization failed, you are not allowed to access this resource');
+    }
+}
+// Export auth middleware
+export default roleMiddleware;
