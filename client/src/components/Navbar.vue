fa169d61bee4901990423aface2b0f9f929c7ada
Bublaaa <yanuarkrisna7@gmail.com>
2023-04-10 03:19:43 +0700

    FE: Create Navbar

---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,160 @@
+<template>
+  <nav class="bg-white border-gray-200 dark:bg-gray-900">
+    <div
+      class="max-w-screen flex flex-wrap items-center justify-between mx-auto p-4"
+    >
+      <!-- Title & logo -->
+      <a href="https://flowbite.com/" class="flex items-center">
+        <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" />
+        <span
+          class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"
+          >Overdose Studio</span
+        >
+      </a>
+
+      <div class="flex items-center my-100">
+        <!-- Search toogle button -->
+        <button
+          @click="toggleSearchdown()"
+          type="button"
+          data-collapse-toggle="navbar-search"
+          aria-controls="navbar-search"
+          aria-expanded="false"
+          class="md:hidden order-2 md:order-2 text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 mr-1"
+        >
+          <svg
+            class="w-5 h-5"
+            aria-hidden="true"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+          <span class="sr-only">Search</span>
+        </button>
+        <!-- Search bar -->
+        <div
+          v-if="isSearchOpen"
+          :class="{ hidden: isMoved }"
+          class="relative right-0 md:right-auto order-3 md:order-2 md:inline"
+        >
+          <div
+            class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none"
+          >
+            <svg
+              class="w-5 h-5 text-gray-500"
+              aria-hidden="true"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+            <span class="sr-only">Search icon</span>
+          </div>
+          <input
+            type="text"
+            id="search-navbar"
+            class="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Search..."
+          />
+        </div>
+        <!-- Profile toggle button -->
+        <button
+          @click="toggleDropdown()"
+          type="button"
+          class="flex ml-5 text-sm bg-gray-800 order-2 md:order-2 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+          id="user-menu-button"
+          aria-expanded="false"
+          data-dropdown-toggle="user-dropdown"
+          data-dropdown-placement="bottom"
+        >
+          <span class="sr-only">Open user menu</span>
+          <img
+            class="w-8 h-8 rounded-full"
+            src="https://flowbite.com/docs/images/logo.svg"
+          />
+        </button>
+        <!-- Profile dropdown -->
+        <div
+          v-if="isDropdownOpen"
+          class="absolute top-20 right-2.5 left-90 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
+          id="user-dropdown"
+        >
+          <div class="px-4 py-3">
+            <span class="block text-sm text-gray-900 dark:text-white"
+              >Bonnie Green</span
+            >
+            <span
+              class="block text-sm text-gray-500 truncate dark:text-gray-400"
+              >name@flowbite.com</span
+            >
+          </div>
+          <ul class="py-2" aria-labelledby="user-menu-button">
+            <li>
+              <a
+                href="#"
+                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                >Dashboard</a
+              >
+            </li>
+            <li>
+              <a
+                href="#"
+                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                >Settings</a
+              >
+            </li>
+            <li>
+              <a
+                href="#"
+                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                >Earnings</a
+              >
+            </li>
+            <li>
+              <a
+                href="#"
+                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                >Sign out</a
+              >
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isDropdownOpen: false,
+      isSearchOpen: true,
+      isMoved: true,
+    };
+  },
+  methods: {
+    toggleSearchdown() {
+      this.isMoved = !this.isMoved;
+      this.isSearchOpen = true;
+    },
+    toggleDropdown() {
+      this.isDropdownOpen = !this.isDropdownOpen;
+    },
+    // toggleSearchdown() {
+    //   this.isSearchOpen = !this.isSearchOpen;
+    // },
+  },
+};
+</script>
--- client/src/main.js
+++ client/src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import './assets/style.css'
 
-createApp(App).mount('#app')
+let app = createApp(App)
+
+
+app.mount('#app')
