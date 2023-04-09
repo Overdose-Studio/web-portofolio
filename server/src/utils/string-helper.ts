c0b3e41043eb5c8ff0c59eb62ed828caf3ad6027
ubeann <76271600+ubeann@users.noreply.github.com>
2023-04-09 10:18:26 +0700

    Create bytesToText function on string helper

--- server/src/utils/string-helper.ts
+++ server/src/utils/string-helper.ts
@@ -2,3 +2,11 @@
 export function toTitleCase(str: string): string {
     return str.toLowerCase().split(' ').map(word => word.charAt(0).toLocaleUpperCase() + word.slice(1)).join(' ');
 }
+
+// Create function bytesToText (KB, MB, GB, TB, etc.)
+export function bytesToText(bytes: number): string {
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+    if (bytes === 0) return '0 Byte';
+    const i = Math.floor(Math.log2(bytes) / 10);
+    return `${(bytes / (1 << (i * 10))).toFixed(2)} ${sizes[i]}`;
+}
