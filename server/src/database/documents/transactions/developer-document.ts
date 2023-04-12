46b84545446a29f78540b44fe84512cd8a78aa6c
ubeann <76271600+ubeann@users.noreply.github.com>
2023-04-12 15:09:16 +0700

    Add education methods on developer model

--- server/src/database/documents/transactions/developer-document.ts
+++ server/src/database/documents/transactions/developer-document.ts
@@ -7,6 +7,8 @@ import { DeveloperType, DeveloperPhotoType } from "../../enums/developer-enum";
 
 // Import interfaces
 import IDeveloperPhoto from "../masters/developer-photo-document";
+import IDeveloperEducation from "../masters/developer-education-document";
+import { IDeveloperEducationRequest } from "../../../interfaces/payload/developer-payload-interface";
 import { SoftDeleteDocument, TimestampDocument } from "../../../interfaces/database/document-database-interface";
 
 // Create a developer document
@@ -26,6 +28,10 @@ interface IDeveloper extends Document, TimestampDocument, SoftDeleteDocument {
     // Methods
     setPhoto(reply: FastifyReply, data: { type: DeveloperPhotoType, path: string}): Promise<IDeveloperPhoto>;
     deletePhoto(reply: FastifyReply, types: DeveloperPhotoType[]): Promise<IDeveloperPhoto>;
+    getEducations(): Promise<IDeveloperEducation[]>;
+    addEducation(data: IDeveloperEducationRequest): Promise<IDeveloperEducation[]>;
+    updateEducation(reply: FastifyReply, data: IDeveloperEducationRequest): Promise<IDeveloperEducation[]>;
+    deleteEducation(reply: FastifyReply, ids: string[]): Promise<IDeveloperEducation[]>;
 }
 
 // Export developer document
